c904d95044dbaabe1b6915d653c5b090e3a9b43b
emiliotorrens <emiliotorrens@hotmail.com>
2012-01-18 16:05:28 +0100

    cambios en tests

--- EtoolTech.MongoDB.Mapper.Test/ConfigTest.cs
+++ EtoolTech.MongoDB.Mapper.Test/ConfigTest.cs
@@ -12,7 +12,6 @@ namespace EtoolTech.MongoDB.Mapper.Test
             Assert.AreEqual(ConfigManager.GetConnectionString("XXX"), "mongodb://192.168.1.214:27017/TestDotNET?connect=direct;maxpoolsize=5;waitQueueTimeout=1000ms;safe=true;fsync=false");
             Assert.AreEqual(ConfigManager.GetConnectionString("Country"), "mongodb://192.168.1.214:27017/TestDotNET?connect=direct;maxpoolsize=5;waitQueueTimeout=1000ms;safe=true;fsync=false");
             Assert.AreEqual(ConfigManager.GetConnectionString("Person"), "mongodb://user:pass@192.168.1.214:27017/TestDotNETPerson?connect=direct;maxpoolsize=10;waitQueueTimeout=2000ms;safe=true;fsync=true");
-
         }
     }
 }
