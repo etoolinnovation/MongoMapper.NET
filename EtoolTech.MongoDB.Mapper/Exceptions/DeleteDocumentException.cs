f0c3ed96c059119abfaea0c21fe179c950710915
Emilio Torrens <emiliotorrens@hotmail.com>
2012-07-11 21:03:20 +0200

    Custom Exceptions para DeleteDocument y ServerUpdate

--- EtoolTech.MongoDB.Mapper/Exceptions/DeleteDocumentException.cs
+++ EtoolTech.MongoDB.Mapper/Exceptions/DeleteDocumentException.cs
@@ -4,5 +4,16 @@ namespace EtoolTech.MongoDB.Mapper.Exceptions
 {
     public class DeleteDocumentException : Exception
     {
+		private readonly string _message;        
+
+        public DeleteDocumentException(string message)
+        {
+            _message = message;            
+        }
+
+        public override string Message
+        {
+            get { return String.Format("Error Deleting Document: {0}", _message); }
+        }
     }
 }
--- EtoolTech.MongoDB.Mapper/Exceptions/ServerUpdateException.cs
+++ EtoolTech.MongoDB.Mapper/Exceptions/ServerUpdateException.cs
@@ -4,5 +4,16 @@ namespace EtoolTech.MongoDB.Mapper.Exceptions
 {
     public class ServerUpdateException : Exception
     {
+		private readonly string _message;        
+
+        public ServerUpdateException(string message)
+        {
+            _message = message;            
+        }
+
+        public override string Message
+        {
+            get { return String.Format("Error Updatind Data: {0}", _message); }
+        }
     }
 }
