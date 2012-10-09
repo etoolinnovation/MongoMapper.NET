8af4fdefa96237539bc88c1ddf418f1704fc1ac4
Emilio Torrens <emilio.torrens@etooltech.com>
2012-10-09 16:49:06 +0200

    INTERNALTOOLS-38 TEST: Posibilidad de subscribirse a un evento en el Current del Enumerador del Driver

--- EtoolTech.MongoDB.Mapper.Test.NUnit/Cursors.cs
+++ EtoolTech.MongoDB.Mapper.Test.NUnit/Cursors.cs
@@ -29,66 +29,23 @@ namespace EtoolTech.MongoDB.Mapper.Test.NUnit
             {
                 countries.OnEnumeratorGetCurrent += Cursors_OnGetCurrent;
             }
+           
 
-            List<Country> c3 = countries.ToList();
-
-            CountryEnumerable countryEnumerable = new CountryEnumerable(countries);
-
-            
-            List<Country> c1 = countryEnumerable.ToList();
-
-            foreach (Country c in countryEnumerable)
+            foreach (Country c in countries)
             {
-                string s = c.Code;
+                Assert.AreEqual("TESTCODE",c.Code);
             }
 
-            List<Country> c2 = countryEnumerable.ToList();
+            
 
         }
 
         void Cursors_OnGetCurrent(object sender, System.EventArgs e)
         {
             Country c = ((MongoCursor<Country>.OnEnumeratorGetCurrentEventArgs)e).Document;
+            c.Code = "TESTCODE";
         }
     }
 
-    public class CountryEnumerable : IEnumerable<Country>
-    {
-        private readonly IEnumerable<Country> _cursor;
-
-        public CountryEnumerable(IEnumerable<Country> cursor) { _cursor = cursor; }
-
-        public IEnumerator<Country> GetEnumerator() { return new CountryEnumerator(_cursor); }
-
-        IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
-    }
-
-    public class CountryEnumerator : IEnumerator<Country>
-    {        
-        private readonly IEnumerator<Country> _enumerator;
-        private Country _current;        
-
-        public CountryEnumerator(IEnumerable<Country> cursor) { _enumerator = cursor.GetEnumerator();}
-
-        public void Dispose() {}
-
-        public bool MoveNext()
-        {
-            while (true)
-            {
-                bool hasNext = _enumerator.MoveNext();
-                if (!hasNext) return false;
-                _current = _enumerator.Current;
-                bool isValid = _current.Code.Contains("1");
-                if (isValid) break;
-            }
-            return true;
-        }
-
-        public void Reset() { _enumerator.Reset(); }
-
-        public Country Current { get { return _current; } }
-
-        object IEnumerator.Current { get { return Current; } }
-    }
+  
 }
