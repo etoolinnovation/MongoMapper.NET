ce80ed6f08a004a5927348d3ddaaa8bb6f3e4560
Emilio Torrens <emilio.torrens@etooltech.com>
2012-10-23 21:15:55 +0200

    Tipando los Extension Methods

--- EtoolTech.MongoDB.Mapper/Core/ExtensionMethods.cs
+++ EtoolTech.MongoDB.Mapper/Core/ExtensionMethods.cs
@@ -14,22 +14,12 @@ namespace EtoolTech.MongoDB.Mapper
         #region Public Methods
 
         public static void Delete<T>(this T o) where T : MongoMapper
-        {
-            if (typeof(T).BaseType != typeof(MongoMapper) && typeof(T) != typeof(MongoMapper))
-            {
-                throw new NotSupportedException();
-            }
-
+        {         
             ((IMongoMapperWriteable)o).Delete<T>();
         }
 
         public static void FillByKey<T>(this T o, params object[] values) where T : MongoMapper
-        {
-            if (typeof(T).BaseType != typeof(MongoMapper))
-            {
-                throw new NotSupportedException();
-            }
-
+        {          
             object result = Finder.Instance.FindByKey<T>(values);
 
             ReflectionUtility.CopyObject(result, o);
@@ -37,11 +27,7 @@ namespace EtoolTech.MongoDB.Mapper
 
         public static void FindByMongoId<T>(this T o, long Id) where T : MongoMapper
         {
-            if (typeof(T) != typeof(MongoMapper) && typeof(T).BaseType != typeof(MongoMapper))
-            {
-                throw new NotSupportedException();
-            }
-
+    
             object result = Finder.Instance.FindById<T>(Id);
 
             ReflectionUtility.CopyObject(result, o);
@@ -49,51 +35,29 @@ namespace EtoolTech.MongoDB.Mapper
 
         public static void MongoFind<T>(this List<T> list, IMongoQuery query = null) where T : MongoMapper
         {
-            if (typeof(T).BaseType != typeof(MongoMapper))
-            {
-                throw new NotSupportedException();
-            }
             list.Clear();
             list.AddRange(query == null ? Finder.Instance.AllAsList<T>() : Finder.Instance.FindAsList<T>(query));
         }
 
         public static void MongoFind<T>(this List<T> list, string fieldName, object value) where T : MongoMapper
         {
-            if (typeof(T).BaseType != typeof(MongoMapper))
-            {
-                throw new NotSupportedException();
-            }
             list.Clear();
             list.AddRange(Finder.Instance.FindAsList<T>(MongoQuery.Eq(fieldName, value)));
         }
 
         public static void MongoFind<T>(this List<T> list, Expression<Func<T, object>> exp) where T : MongoMapper
         {
-            if (typeof(T).BaseType != typeof(MongoMapper))
-            {
-                throw new NotSupportedException();
-            }
             list.Clear();
             list.AddRange(Finder.Instance.FindAsList(exp));
         }
 
         public static int Save<T>(this T o) where T : MongoMapper
-        {
-            if (typeof(T).BaseType != typeof(MongoMapper) && typeof(T) != typeof(MongoMapper))
-            {
-                throw new NotSupportedException();
-            }
-
+        {   
             return ((IMongoMapperWriteable)o).Save<T>();
         }
 
         public static void ServerUpdate<T>(this T o, UpdateBuilder update, bool refill = true) where T : MongoMapper
-        {
-            if (typeof(T).BaseType != typeof(MongoMapper))
-            {
-                throw new NotSupportedException();
-            }
-
+        {         
             ((IMongoMapperWriteable)o).ServerUpdate<T>(update, refill);
         }
 
