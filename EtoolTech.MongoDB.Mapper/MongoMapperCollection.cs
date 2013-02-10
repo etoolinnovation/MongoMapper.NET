4ec4499334753ea1d8feabfdbda12a683d388064
Emilio Torrens <emilio.torrens@etooltech.com>
2013-02-10 12:47:43 +0100

    Actualizados todos los Test con la nueva busqueda

--- EtoolTech.MongoDB.Mapper/MongoMapperCollection.cs
+++ EtoolTech.MongoDB.Mapper/MongoMapperCollection.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using EtoolTech.MongoDB.Mapper.Configuration;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace EtoolTech.MongoDB.Mapper
