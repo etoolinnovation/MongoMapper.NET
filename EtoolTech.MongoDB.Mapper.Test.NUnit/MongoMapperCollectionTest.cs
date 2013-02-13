7590a38a8692928876b008dd5a9036251cab60b1
Emilio Torrens <emilio.torrens@etooltech.com>
2013-02-13 13:13:45 +0100

    MongoMapperCollection

--- EtoolTech.MongoDB.Mapper.Test.NUnit/MongoMapperCollectionTest.cs
+++ EtoolTech.MongoDB.Mapper.Test.NUnit/MongoMapperCollectionTest.cs
@@ -9,46 +9,9 @@ namespace EtoolTech.MongoDB.Mapper.Test.NUnit
     [TestFixture]
     public class MongoMapperCollectionTest
     {
+      
         [Test]
         public void Test()
-        {
-            Helper.DropAllCollections();
-            
-            var country = new Country { Code = "NL", Name = "Holanda" };
-            country.Save();
-            country = new Country { Code = "UK", Name = "Reino Unido" };
-            country.Save();
-            country = new Country { Code = "ES", Name = "España" };
-            country.Save();
-            
-            var col = new MongoMapperCollection<Country>();
-            col.Find().SetLimit(1);
-
-            Console.WriteLine(col.Cursor.Explain().ToJson());
-
-            Assert.AreEqual(1, col.Count);
-            Assert.AreEqual(3, col.Total);
-
-            col = new MongoMapperCollection<Country>();
-            col.Find().SetLimit(3).SetSortOrder("Name");
-
-            Assert.AreEqual(3, col.Count);
-            Assert.AreEqual("ES",col.First().Code);
-
-            col.Find(Query<Country>.EQ(C => C.Code, "NL"));
-
-            Assert.AreEqual("NL",col.First().Code);
-            Assert.AreEqual("NL", col.Last().Code);
-
-            foreach (Country c in col)
-            {
-                
-            }
-
-        }
-
-        [Test]
-        public void Test2()
         {
             Helper.DropAllCollections();
 
