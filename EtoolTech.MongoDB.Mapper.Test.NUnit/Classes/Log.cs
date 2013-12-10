c7c06a36e7868f1d7811b5560fa8de488f013021
Emilio Torrens <emilio.torrens@etooltech.com>
2013-12-10 12:50:40 +0100

    Added MongoTTLIndex Attribute

---
+++ EtoolTech.MongoDB.Mapper.Test.NUnit/Classes/Log.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EtoolTech.MongoDB.Mapper.Attributes;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EtoolTech.MongoDB.Mapper.Test.NUnit
+{
+    [Serializable]
+    [MongoKey(KeyFields = "")]
+    [MongoMapperIdIncrementable(IncremenalId = true, ChildsIncremenalId = false)]
+    [MongoTTLIndex(IndexField = "Date",Seconds = 600)]
+    public class Log: MongoMapper
+    {
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime Date { get; set; }
+        public string Data { get; set; }
+    }
+}
